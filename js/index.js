cd65a8ae1dd06adea1bfa115e3777fe932c8d57b
domo-fernaz <97562935+domo-fernaz@users.noreply.github.com>
2022-01-12 14:21:06 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,9 +7,18 @@ function obj(x, y, w, h) {
   this.w = w;
   this.h = h;
   this.draw = function () {
+    ctx.fillColor = "#FFFFFF";
     ctx.fillRect(this.x, this.y, this.w, this.h);
   };
 }
 
 let player = new obj(0, 0, 10, 25);
 let ball = new obj(390, 250, 10, 10);
+
+setInterval(() => {
+  ctx.fillColor = "#000000";
+  ctx.fillRect(0, 0, 800, 600);
+
+  ball.draw();
+  player.draw();
+}, 17);
