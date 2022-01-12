ea4af43fa359b2d1d5d02614a62203ff50b2027d
domo-fernaz <97562935+domo-fernaz@users.noreply.github.com>
2022-01-12 14:14:56 -0800

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 let canvas = document.getElementById("candice");
 let ctx = canvas.getContext("2d");
 
-//Code
-ctx.fillColor = "#000000";
-ctx.fillRect(0, 0, 10, 10);
+function obj(x, y, w, h) {
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.draw = function () {
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  };
+}
+
+let player = new obj(0, 0, 10, 25);
+let ball = new obj(390, 250, 10, 10);
